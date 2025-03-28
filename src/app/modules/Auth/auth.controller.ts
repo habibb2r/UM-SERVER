f41a124eefa2bc2a3ab969041538d0cf81c80552
habibb2r <habibhk127@gmail.com>
2025-03-28 23:22:44 +0600

    updated

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -40,6 +40,7 @@ const changePassword = catchAsync(async (req, res) => {
 });
 
 const refreshToken = catchAsync(async (req, res) => {
+
   const { refreshToken } = req.cookies;
   const result = await AuthServices.refreshToken(refreshToken);
 
